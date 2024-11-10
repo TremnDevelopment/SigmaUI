c15de583f23b07460f0a35f3a59197d95bbfb634
KeyToFailure <juliagirlhehe@gmail.com>
2024-11-10 12:00:04 -1000

    Update SigmaLibrary.lua

--- SigmaLibrary.lua
+++ SigmaLibrary.lua
@@ -211,13 +211,4 @@ function UI:CreateSlider(parent, min, max, initialValue, callback)
     return slider
 end
 
-function UI:ShowSectionForTab(tab, sections)
-    for _, section in pairs(sections) do
-        section.Visible = false
-    end
-    if tab and sections[tab] then
-        sections[tab].Visible = true
-    end
-end
-
 return UI
